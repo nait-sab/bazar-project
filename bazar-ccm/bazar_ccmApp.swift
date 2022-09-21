0380ec8c826094b086c39a6d5f8985030578633c
= <=>
2022-09-21 14:34:00 +0200

    Initial Commit

---
+++ bazar-ccm/bazar_ccmApp.swift
@@ -0,0 +1,17 @@
+//
+//  bazar_ccmApp.swift
+//  bazar-ccm
+//
+//  Created by etudiant on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct bazar_ccmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
